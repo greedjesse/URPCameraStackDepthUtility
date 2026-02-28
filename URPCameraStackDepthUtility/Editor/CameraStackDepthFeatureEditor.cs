7346c2e04b5e1a749ea2d98de7e38c5516439e5e
greedjesse <67365952+greedjesse@users.noreply.github.com>
2026-02-28 13:04:56 +0800

    Add CameraStackDepthFeature editor
    Add a custom inspector (CameraStackDepthFeatureEditor) under URPCameraStackDepthUtility/Editor to manage a "Selected Cameras" boolean list. Displaying the actual camera names in the inspector.

---
+++ URPCameraStackDepthUtility/Editor/CameraStackDepthFeatureEditor.cs
@@ -0,0 +1,57 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(CameraStackDepthFeature))]
+public class CameraStackDepthFeatureEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        DrawPropertiesExcluding(serializedObject, "m_SelectedCameras");
+
+        EditorGUILayout.Space(10);
+        EditorGUILayout.LabelField("Selected Cameras", EditorStyles.boldLabel);
+
+        var feature = (CameraStackDepthFeature)target;
+        var selectedProp = serializedObject.FindProperty("m_SelectedCameras");
+
+        var mainCamera = Camera.main;
+        if (!mainCamera)
+        {
+            EditorGUILayout.HelpBox("No Main Camera found.", MessageType.Warning);
+            serializedObject.ApplyModifiedProperties();
+            return;
+        }
+
+        var additionalData = mainCamera.GetUniversalAdditionalCameraData();
+
+        List<Camera> stack = new List<Camera>();
+        stack.Add(mainCamera);
+        stack.AddRange(additionalData.cameraStack);
+
+        // Ensure size matches stack
+        while (selectedProp.arraySize < stack.Count)
+            selectedProp.InsertArrayElementAtIndex(selectedProp.arraySize);
+
+        while (selectedProp.arraySize > stack.Count)
+            selectedProp.DeleteArrayElementAtIndex(selectedProp.arraySize - 1);
+
+        EditorGUI.indentLevel++;
+
+        for (int i = 0; i < stack.Count; i++)
+        {
+            var element = selectedProp.GetArrayElementAtIndex(i);
+            var cam = stack[i];
+
+            string label = cam ? cam.name : "Missing Camera";
+            element.boolValue = EditorGUILayout.ToggleLeft(label, element.boolValue);
+        }
+
+        EditorGUI.indentLevel--;
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
